9e1e9a0d0817aeb425c8be1f87ce178d6809fb64
Md Asif <142498969+asifreza69@users.noreply.github.com>
2026-04-25 23:55:36 +0530

    Add solution for Koko Eating Bananas problem
    Implemented binary search to determine the minimum eating speed for Koko to finish all bananas within given hours.

---
+++ Binary Search/koko_eating_bananas.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canEat(vector<int>& piles, int speed, int h) {
+        long long hours = 0;
+
+        for (int bananas : piles) {
+            hours += (bananas + speed - 1) / speed; // ceil
+        }
+
+        return hours <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canEat(piles, mid, h)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
+Problem: Koko Eating Bananas (LC 875)
+
+Approach:
+Binary Search on Answer
+
+Time Complexity: O(n log max(pile))
+Space Complexity: O(1)
+
+Key Insight:
+Search minimum speed such that total hours <= h
+*/
